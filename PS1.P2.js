1a081a567f5a94bfafb6c2a66ce0de9394e1c9b4
jonathanwhang <42430020+jonathanwhang@users.noreply.github.com>
2020-02-05 01:24:49 -0500

    Update PS1.P2.js

--- PS1.P2.js
+++ PS1.P2.js
@@ -3,38 +3,31 @@ const evaluate = expr => {
     switch (op)
     {
         case '+':
-            return (x,y) => x+y;
+            return (expr) => parseInt(expr[0]) + parseInt(expr[2]);
 
         case '-':
-            return (x,y) => x-y;
+            return (expr) => parseInt(expr[0]) - parseInt(expr[2]);
 
         case '*':
-            return (x,y) => x*y;
+            return (expr) => parseInt(expr[0]) * parseInt(expr[2]);
 
         case '/':
-            return (x,y) => x/y;
+            return (expr) => parseInt(expr[0]) / parseInt(expr[2]);
 
         case '%':
-            return (x,y) => x%y;
+            return (expr) => parseInt(expr[0]) % parseInt(expr[2]);
 
         default:
             return;
     }
 }
 
-const perform = expr => {
-    let func = evaluate(expr);
-    let op1 = parseInt(expr.charAt(0));
-    let op2 = parseInt(expr.charAt(2));
-
-    return func(op1,op2);
-}
-
 for (var expr of ['4+2', '5*7', '6-1', '9/2'])
 {
-    console.log(`${expr} = ${perform(expr)}`);
+    let operator = evaluate(expr);
+    console.log(`${expr} = ${operator(expr)}`);
 }
 
-const expression = '8%3';
-let operator = evaluate(expression);
-// console.log(`${expression} = ${perform(expression)}`);
+// const expression = '8%3';
+// let operator = evaluate(expression);
+// console.log(`${expression} = ${operator(expression)}`);
